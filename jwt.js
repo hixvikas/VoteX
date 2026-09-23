792133357843f479d6b4cb68db7868976ae8b24e
Vikas <sharmavikas12909@gmail.com>
2026-09-23 12:52:59 +0530

    Fixes the JWT token related bugs.

--- jwt.js
+++ jwt.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = process.env.JWT_SECRET || 'votex_fallback_jwt_secret_key';
+
 const jwtAuthMiddleware = (req, res, next) => {
 
     //first check request headers has authorization or not 
@@ -12,7 +14,7 @@ const jwtAuthMiddleware = (req, res, next) => {
 
     try {
         // verify the jwt token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET); 
+        const decoded = jwt.verify(token, JWT_SECRET); 
         
         // Attach user information to the request object
         req.user = decoded     // we can put anything at user like userEncodedData etc..
@@ -29,7 +31,7 @@ const jwtAuthMiddleware = (req, res, next) => {
 const generateToken = (userData) => {
     // generate a new JWT token using user data
 
-    return jwt.sign(userData, process.env.JWT_SECRET);   // we can put expiry time also , {expiresIn: 300000000}
+    return jwt.sign(userData, JWT_SECRET);   // we can put expiry time also , {expiresIn: 300000000}
 }
 
 module.exports = {jwtAuthMiddleware, generateToken}
